6f886cd95b6596220fa505c4d7eefc16082a4e02
rodimirv <83525769+rodimirv@users.noreply.github.com>
2021-05-12 16:10:33 -0700

    Update fAME.py

--- fAME.py
+++ fAME.py
@@ -7,9 +7,11 @@
 # Revision 2	- May 4, 2021	- Added Swapfile
 # Revision 2.1	- May 7, 2021	- Added Tkinter install when Tkinter does not exist in the device
 #				- Added print colors
+# Revision 2.2	- May 12, 2021	- Added folder template creation for jetson-inference/python/training/detection/ssd
 
 import time
 from subprocess import call
+import os
 import getpass
 
 class bcolors:
@@ -149,6 +151,17 @@ while True:
 			call(['gnome-terminal', '--working-directory=/home/' +currentUser+ '/gui-inference/installSwapfile', '--', 'bash', 'installSwapfile.sh'])
 
 		# Root Controls
+		def btnCusMdlFolder_click():
+			crFolderName = crInput1.get()
+			crRow = btnCusMdlFolder.grid_info()['row']
+			if (crFolderName != ''):
+				call('cp -r "custom model template"  ~/jetson-inference/python/training/detection/ssd/data/'+crFolderName, shell=True)
+				crLabelErr = Label(root, text='Folder name "' +crFolderName+ '" created!', fg="green", bg='white')
+				crLabelErr.grid(row=crRow+1, column=0, columnspan=8)
+			else:
+				crLabelErr = Label(root, text=folderEmpty, fg="red", bg='white')
+				crLabelErr.grid(row=crRow+1, column=0, columnspan=8)
+		
 		def btnCap_click():
 			cameraDir = camDirInput.get()
 			call(['gnome-terminal', '--', 'camera-capture', '/dev/' +cameraDir])
@@ -162,18 +175,26 @@ while True:
 			batchSize = trInput2.get()
 			workers = trInput3.get()
 			epoch = trInput4.get()
+			trRow = btnTrain.grid_info()['row']
 			if (folderName != ''):
 				call(['gnome-terminal', '--working-directory=/home/' +currentUser+ '/jetson-inference/python/training/detection/ssd', '--command=python3 train_ssd.py --dataset-type=voc --data=data/' +folderName+ ' --model-dir=models/' +folderName+ ' --batch-size=' +batchSize+ ' --workers=' +workers+ ' --epoch=' +epoch])
+				trainLabel = Label(root, text='Traning custom annotated model... see terminal for progress', fg="green", bg='white')
+				trainLabel.grid(row=trRow+1, column=0, columnspan=8)
 			else:
-				trainLabel.grid(row=10, column=0, columnspan=8)
+				trainLabel = Label(root, text=folderEmpty, fg="red", bg='white')
+				trainLabel.grid(row=trRow+1, column=0, columnspan=8)
 
 		def btnExport_click():
 			global foldername
 			folderName = trInput1.get()
+			exRow = btnExport.grid_info()['row']
 			if (folderName != ''):
 				call(['gnome-terminal', '--working-directory=/home/' +currentUser+ '/jetson-inference/python/training/detection/ssd', '--command=python3 onnx_export.py --model-dir=models/' +folderName])
+				exportLabel = Label(root, text='Exporting trained models to ONNX... Please see terminal for progress', fg="green", bg='white')
+				exportLabel.grid(row=exRow+1, column=0, columnspan=8)
 			else:
-				exportLabel.grid(row=12, column=0, columnspan=8)
+				exportLabel = Label(root, text=folderEmpty, fg="red", bg='white')
+				exportLabel.grid(row=exRow+1, column=0, columnspan=8)
 
 		# Buttons, Entry, and Labels
 
@@ -181,7 +202,14 @@ while True:
 
 		btnInstallers = Button(root, text="Installers", command=btnInstallers_click, padx=58, bg='white', highlightbackground = hlColor)
 
-		cameraCaptureLabel = Label(root, text='Enter device directory | e.g. /dev/video0' + '\nUse the "Show List of Cameras Detected" button if unknown', bg='white')
+		CusMdlLabel = Label(root, text='\nCopy a blank template to the jetson-inference training folder \nwhere all your annotations, imagesets, JPEGimages, and labels.txt will be located and saved', bg='white')
+
+		crInputLabel1 = Label(root, text='       Name of the Folder to be Created:', bg='white')
+		crInput1 = Entry(root, width=10, bg='white', highlightbackground = hlColor)
+
+		btnCusMdlFolder = Button(root, text="Copy Template to the Jetson Training Folder", command=btnCusMdlFolder_click, padx=10, bg='white', highlightbackground = hlColor)
+
+		cameraCaptureLabel = Label(root, text='\nEnter device directory | e.g. /dev/video0' + '\nUse the "Show List of Cameras Detected" button if unknown', bg='white')
 
 		camDirInputLabel = Label(root, text='Camera Directory:   /dev/', bg='white')
 
@@ -191,12 +219,13 @@ while True:
 
 		btnShowCamLst = Button(root, text="Show List of Cameras Detected", command=btnShowCamLst_click, bg='white', highlightbackground = hlColor)
 
-		trLabel1 = Label(root, text='Set the following requirements then click on the "Train Annotated Models" button to start training custom model', pady=10, bg='white')
+		trLabel1 = Label(root, text='\nSet the following requirements then click on the "Train Annotated Models" button to start training custom model', bg='white')
+
 		trInputLabel1 = Label(root, text='Folder Name:', bg='white')
 		trInputLabel2 = Label(root, text='Batch Size:', bg='white')
 		trInputLabel3 = Label(root, text='Workers:', bg='white')
 		trInputLabel4 = Label(root, text='Epoch:', bg='white')
-
+		
 		trInput1 = Entry(root, width=10, bg='white', highlightbackground = hlColor)
 		trInput2 = Entry(root, width=10, bg='white', highlightbackground = hlColor)
 		trInput3 = Entry(root, width=10, bg='white', highlightbackground = hlColor)
@@ -208,33 +237,32 @@ while True:
 
 		btnTrain = Button(root, text="Train Annotated Models", command=btnTrain_click, bg='white', highlightbackground = hlColor)
 
-
-		trainLabel = Label(root, text=folderEmpty, fg="red", bg='white')
-
 		btnExport = Button(root, text="Export Trained Models to ONNX", command=btnExport_click, bg='white', highlightbackground = hlColor)
-
-		exportLabel = Label(root, text=folderEmpty, fg="red", bg='white')
 
 		# Show Labels,Entry, and Buttons
 		banner.grid(row=1, column=0, columnspan=8)
 		installersLabel.grid(row=2, column=0, columnspan=8)
 		btnInstallers.grid(row=3, column=0, columnspan=8)
-		cameraCaptureLabel.grid(row=4, column=0, columnspan=8)
-		camDirInputLabel.grid(row=5, column=0, columnspan=2)
-		camDirInput.grid(row=5, column=0, columnspan=5)
-		btnCap.grid(row=5, column=0, columnspan=8)
-		btnShowCamLst.grid(row=5, column=5, columnspan=8)
-		trLabel1.grid(row=6, column=0, columnspan=8)
-		trInputLabel1.grid(row=8, column=0)
-		trInputLabel2.grid(row=8, column=2)
-		trInputLabel3.grid(row=8, column=4)
-		trInputLabel4.grid(row=8, column=6)
-		trInput1.grid(row=8, column=1)
-		trInput2.grid(row=8, column=3)
-		trInput3.grid(row=8, column=5)
-		trInput4.grid(row=8, column=7)
-		btnTrain.grid(row=9, column=0, columnspan=8)
-		btnExport.grid(row=11, column=0, columnspan=8)
+		CusMdlLabel.grid(row=4, column=0, columnspan=8)
+		crInputLabel1.grid(row=6, column=0, columnspan=3)
+		crInput1.grid(row=6, column=0, columnspan=7)
+		btnCusMdlFolder.grid(row=6, column=3, columnspan=8)
+		cameraCaptureLabel.grid(row=8, column=0, columnspan=8)
+		camDirInputLabel.grid(row=9, column=0, columnspan=2)
+		camDirInput.grid(row=9, column=0, columnspan=5)
+		btnCap.grid(row=9, column=0, columnspan=8)
+		btnShowCamLst.grid(row=9, column=5, columnspan=8)
+		trLabel1.grid(row=10, column=0, columnspan=8)
+		trInputLabel1.grid(row=12, column=0)
+		trInputLabel2.grid(row=12, column=2)
+		trInputLabel3.grid(row=12, column=4)
+		trInputLabel4.grid(row=12, column=6)
+		trInput1.grid(row=12, column=1)
+		trInput2.grid(row=12, column=3)
+		trInput3.grid(row=12, column=5)
+		trInput4.grid(row=12, column=7)
+		btnTrain.grid(row=13, column=0, columnspan=8)
+		btnExport.grid(row=16, column=0, columnspan=8)
 
 
 		root.mainloop()
